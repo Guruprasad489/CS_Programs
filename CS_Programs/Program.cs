a042f961af7d75f453cfdbac53a4532dd81a5578
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-12 19:07:26 +0530

    Adding FibonacciSreies, ReverseNumber and ReverseString programs

---
+++ CS_Programs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //FibonacciSeries f = new FibonacciSeries();  //To create object
+            //FibonacciSeries.FindFibonacciSeries();
+            //ReverseNumber.FindReverseNumber();
+            ReverseString.GetReverseString();
+        }
+    }
+}
---
+++ CS_Programs/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programs
+{
+    public class ReverseNumber
+    {
+        public static void FindReverseNumber()
+        {
+            int reminder, reverse=0;
+            Console.WriteLine("Please Enter Number to reverse");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            while (number > 0)
+            {
+                reminder = number % 10;
+                reverse = (reverse * 10) + reminder;
+                number = number / 10;
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+
+    }
+}
