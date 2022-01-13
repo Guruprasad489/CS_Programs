7c90daa24111f23a0be6bc93a81d97044f952f6c
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-13 13:58:01 +0530

    Adding SwapNumbers.cs

--- CS_Programs/Program.cs
+++ CS_Programs/Program.cs
@@ -11,9 +11,28 @@ namespace CS_Programs
         static void Main(string[] args)
         {
             //FibonacciSeries f = new FibonacciSeries();  //To create object
-            //FibonacciSeries.FindFibonacciSeries();
-            //ReverseNumber.FindReverseNumber();
-            ReverseString.GetReverseString();
+            Console.WriteLine("Choose the program");
+            Console.WriteLine("\n 1. Fibonacci Series \n 2. Reverse Number \n 3. Reverse String \n 4. Swap Numbers ");
+            //int programNum = Convert.ToInt32(Console.ReadLine()); //To read integer value
+            string programNum = Console.ReadLine();
+            switch (programNum)
+            {
+                case "1": 
+                    FibonacciSeries.FindFibonacciSeries();
+                    break;
+                case "2":
+                    ReverseNumber.FindReverseNumber();
+                    break;
+                case "3":
+                    ReverseString.GetReverseString();
+                    break;
+                case "4":
+                    SwapNumbers.SwapingNumbers();
+                    break;
+                default:
+                    Console.WriteLine("Please choose the correct Program");
+                    break;
+            }
         }
     }
 }
---
+++ CS_Programs/SwapNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programs
+{
+    public class SwapNumbers
+    {
+        //Each word 1st letter has to start with Uppercase //Pascal case
+        //Pascal will follow for classname and method name
+        public static void SwapingNumbers()
+        {
+            //camel case for local variables and method parameters
+            int firstNum, secondNum, temp = 0;
+            Console.WriteLine("Enter First Number");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swaping: First Number is {0}, Second Number is {1}", firstNum, secondNum);
+            temp = firstNum;
+            firstNum=secondNum;
+            secondNum=temp;
+            Console.WriteLine("Before swaping: First Number is {0}, Second Number is {1}", firstNum, secondNum);
+        }
+    }
+}
